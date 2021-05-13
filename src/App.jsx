5100d3780c0e6cc6eb553b124b2aa04ba15d232b
James Bissick <bissickj@gmail.com>
2021-05-13 21:28:16 +0200

    ✨ - Feat: Adding header component, links to home, shop page and contact
    I had to use the vite-plugin-react-svg in order to load the logo svg because the old way to do so was using create-react-app, and we use Vite in this project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <>
+      <Header />
       <Switch>
         <Route exact path="/" component={ Homepage } />
         <Route path="/shop" component={ ShopPage } />
