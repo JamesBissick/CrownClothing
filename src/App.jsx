2827980b8c61307f8433e9ab45b5f15c5e59a1bc
James Bissick <bissickj@gmail.com>
2021-05-13 22:44:42 +0200

    ✨ - Feat: Adding Custom button logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import LoginRegisterPage from './pages/login-register/login-register.component';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ Homepage } />
         <Route path="/shop" component={ ShopPage } />
+        <Route path="/login" component={ LoginRegisterPage } />
       </Switch>
     </>
   );
