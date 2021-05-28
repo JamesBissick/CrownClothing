af4f3156adc6b9fa764a488deca4135e039b1fe7
James Bissick <bissickj@gmail.com>
2021-05-28 12:33:32 +0200

    ✨ - Feat: Added Redux, and implemented useReducer and userActions to header app components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import Logo from '../../assets/crown.svg?component'; // Used the vite-plugin-react-svg package
@@ -6,18 +7,23 @@ import Logo from '../../assets/crown.svg?component'; // Used the vite-plugin-rea
 import './header.styles.scss';
 
 const Header = ({ currentUser }) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
-      <Logo className="logo" />
+  <div className='header'>
+    <Link className='logo-container' to='/'>
+      <Logo className='logo' />
     </Link>
-    <div className="options">
-      <Link className="option" to="/shop">SHOP</Link>
-      <Link className="option" to="/shop">CONTACT</Link>
+    <div className='options'>
+      <Link className='option' to='/shop'>SHOP</Link>
+      <Link className='option' to='/shop'>CONTACT</Link>
       {
-        currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/login'>SIGN IN</Link>
+        currentUser ? <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div> :
+          <Link className='option' to='/login'>SIGN IN</Link>
       }
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
