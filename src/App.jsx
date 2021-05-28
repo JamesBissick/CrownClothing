a2535a7d15e08caafa354cf56b57cf0d9209f827
James Bissick <bissickj@gmail.com>
2021-05-28 20:30:14 +0200

    ✨ - Feat: Redirect to Home if user is logged in

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component, useState } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -45,15 +45,20 @@ class App extends Component {
         <Switch>
           <Route exact path='/' component={ Homepage } />
           <Route path='/shop' component={ ShopPage } />
-          <Route path='/login' component={ LoginRegisterPage } />
+          <Route exact path='/login'
+                 render={ () => this.props.currentUser ? (<Redirect to='/' />) : (<LoginRegisterPage />) } />
         </Switch>
       </>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
