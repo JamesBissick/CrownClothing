08b259e7e018457d4bc529d1e4126101c9ee3e78
James Bissick <bissickj@gmail.com>
2021-06-01 11:25:33 +0200

    ✨ - Feat: Added cartIcon and cart component/reducer and dropdown cart

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
+
 import Logo from '../../assets/crown.svg?component'; // Used the vite-plugin-react-svg package
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -18,12 +21,17 @@ const Header = ({ currentUser }) => (
         currentUser ? <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div> :
           <Link className='option' to='/login'>SIGN IN</Link>
       }
+      <CartIcon />
     </div>
+    {
+      hidden ? null : <CartDropdown />
+    }
   </div>
 );
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
