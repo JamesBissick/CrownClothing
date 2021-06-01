db2b100f3a4f0e9120ec7c403ff55346ca4ed044
James Bissick <bissickj@gmail.com>
2021-06-01 16:33:26 +0200

    ⚡️ - Performance: Refactor code structure to use the createStructuredSelector method from reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
+import { createStructuredSelector } from 'reselect';
 
 import Logo from '../../assets/crown.svg?component'; // Used the vite-plugin-react-svg package
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './header.styles.scss';
 
@@ -29,9 +32,9 @@ const Header = ({ currentUser, hidden }) => (
   </div>
 );
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,12 +2,17 @@ import { createSelector } from 'reselect';
 
 const selectCart = state => state.cart;
 
+export const selectCartHidden = createSelector(
+  [ selectCart ],
+  cart => cart.hidden
+);
+
 export const selectCartItems = createSelector(
   [ selectCart ],
   (cart) => cart.cartItems
 );
 
 export const selectCartItemsCount = createSelector(
-  [selectCartItems],
+  [ selectCartItems ],
   cartItems => cartItems.reduce((accumulatedQuantity, cardItem) => accumulatedQuantity + cardItem.quantity, 0)
-)
+);
